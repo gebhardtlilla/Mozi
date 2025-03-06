e695377463f54c2026d6ccca885c2a914800e18a
Gebhardt Lilla <gebhardt.lilla@ckik.loc>
2025-03-06 14:05:15 +0100

    0306

--- mozibeadando/mozibeadando/Terem.cs
+++ mozibeadando/mozibeadando/Terem.cs
@@ -7,20 +7,28 @@ using System.Threading.Tasks;
 
 namespace Mozizas
 {
-    public class Terem:ITerem
+    public class Terem : ITerem
     {
         public string TeremNev { get; set; }
         public int TeremFerohely { get; set; }
+        public int TeremHossza { get; set; }
+        public int SzekPerSor { get; set; }
 
         /// <summary>
-        /// A teremhez hozzárendeli a nevét és azt hogy hány férőhely van benne. (A terem neveit már a musorlista osztályba hoztuk létre.)
+        /// A teremhez hozzárendeli a nevét, férőhelyét, hosszát és soronkénti székszámát.
         /// </summary>
-        /// <param name="TeremNev">A terem neve(terem1, terem2, terem3)</param>
-        public Terem(string TeremNev)
+        /// <param name="TeremNev">A terem neve (pl. terem1, terem2, terem3).</param>
+        /// <param name="TeremFerohely"></param>
+        /// <param name="TeremHossza"></param>
+        /// <param name="SzekPerSor"></param>
+        public Terem(string TeremNev, int TeremFerohely, int TeremHossza, int SzekPerSor)
         {
             Random r = new Random();
             this.TeremNev = TeremNev;
             TeremFerohely = r.Next(80, 350);
+            TeremHossza = r.Next(15, 30); 
+            SzekPerSor = 15;  
         }
     }
 }
+
